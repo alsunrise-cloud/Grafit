f0790a86a2a01cbe24667b958e028ad9f8c7a32c
alsunrise-cloud <alsunrise@bk.ru>
2026-06-01 04:17:50 +0700

    Fix Render database connection

--- config/db.php
+++ config/db.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = "dpg-d8d1cq3bc2fs73eodgsg-a.oregon-postgres.render.com";
+$host = "dpg-d8d1cq3bc2fs73eodgsg-a";
 $port = "5432";
 $dbname = "grafit_z5oy";
 $user = "grafit_user";
-$password = "mf1cDHRrv1XlGQfBobDmW29BbruZatXp";
+$password = "ТВОЙ_ПАРОЛЬ";
 
 try {
     $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
+        "pgsql:host=$host;port=$port;dbname=$dbname",
         $user,
         $password
     );
